d644d5060fca0bdd5c15ebcdac5a565400035d2a
Ronan <ronanir600@gmail.com>
2015-11-23 16:02:13 +0000

    referral

---
+++ Celtic Rugby Tours_files/main.js
@@ -0,0 +1,282 @@
+// $(function() {
+
+// // ***********************************
+// 		 // Location Button
+// // ************************************
+// 	$('.home-button').on('click', function() {
+// 		$('.location').css({
+// 			'color' : 'white', 
+// 			'background-color' : 'green'
+// 		})
+// 	})
+
+// 	$('.home-button').on('click', function() {
+// 		$('.about-button').css({
+// 			'background-color' : 'none'
+// 		})
+// 	})
+
+// 	$('.home-button').on('click', function() {
+// 		$('.tours-button').css({
+// 			'background-color' : 'none'
+// 		})
+// 	})
+
+// 	$('.home-button').on('click', function() {
+// 		$('.attractions-button').css({
+// 			'background-color' : 'none'
+// 		})
+// 	})
+
+// 	$('.last').on('click', function() {
+// 		$('.other').css({
+// 			'background-color' : 'none'
+// 		})
+// 	})
+
+// 	$('.home-button').on('click', function() {
+// 		$('.rsvp').css({
+// 			'background-color' : 'none'
+// 		})
+// 	})
+
+// // ***********************************
+// 		 // Accommodation Button
+// // ************************************
+
+
+// 	$('.about-button').on('click', function() {
+// 		$('.about-button').css({
+// 			'color' : 'white', 
+// 			'background-color' : 'green',
+// 			'padding-left' : '13px',
+
+// 		})
+// 	})
+
+// 	$('.about-button').on('click', function() {
+// 		$('.home-button').css({
+// 			'color' : 'white', 
+// 			'background-color' : 'none'
+// 		})
+// 	})
+
+// 	$('.about-button').on('click', function() {
+// 		$('.flights').css({
+// 			'color' : 'green', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.about-button').on('click', function() {
+// 		$('.holiday').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.about-button').on('click', function() {
+// 		$('.other').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.about-button').on('click', function() {
+// 		$('.rsvp').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+
+// // ***********************************
+// 		 // Flights Button
+// // ************************************
+
+
+// 	$('.flights').on('click', function() {
+// 		$('.flights').css({
+// 			'color' : 'white', 
+// 			'background-color' : '#de00a5'
+// 		})
+// 	})
+
+// 	$('.flights').on('click', function() {
+// 		$('.location').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.flights').on('click', function() {
+// 		$('.accommodation').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.flights').on('click', function() {
+// 		$('.holiday').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.flights').on('click', function() {
+// 		$('.other').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.flights').on('click', function() {
+// 		$('.rsvp').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// // ***********************************
+// 		 // Flights Button
+// // ************************************
+
+
+// 	$('.holiday').on('click', function() {
+// 		$('.holiday').css({
+// 			'color' : 'white', 
+// 			'background-color' : '#de00a5'
+// 		})
+// 	})
+
+// 	$('.holiday').on('click', function() {
+// 		$('.location').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.holiday').on('click', function() {
+// 		$('.accommodation').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.holiday').on('click', function() {
+// 		$('.flights').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.holiday').on('click', function() {
+// 		$('.other').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.holiday').on('click', function() {
+// 		$('.rsvp').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// // ***********************************
+// 		 // RSVP Button
+// // ************************************
+
+
+// 	$('.rsvp').on('click', function() {
+// 		$('.rsvp').css({
+// 			'color' : 'white', 
+// 			'background-color' : '#de00a5'
+// 		})
+// 	})
+
+// 	$('.rsvp').on('click', function() {
+// 		$('.location').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.rsvp').on('click', function() {
+// 		$('.accommodation').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.rsvp').on('click', function() {
+// 		$('.flights').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+
+// 	$('.rsvp').on('click', function() {
+// 		$('.other').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.rsvp').on('click', function() {
+// 		$('.holiday').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// // ***********************************
+// 		 // Other
+// // ************************************
+
+
+// 	$('.other').on('click', function() {
+// 		$('.other').css({
+// 			'color' : 'white', 
+// 			'background-color' : '#de00a5'
+// 		})
+// 	})
+
+// 	$('.other').on('click', function() {
+// 		$('.location').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.other').on('click', function() {
+// 		$('.accommodation').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.other').on('click', function() {
+// 		$('.flights').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.other').on('click', function() {
+// 		$('.holiday').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// 	$('.other').on('click', function() {
+// 		$('.rsvp').css({
+// 			'color' : '#de00a5', 
+// 			'background-color' : 'white'
+// 		})
+// 	})
+
+// })
